b1344f87c7fe1d97165baeb55b7ad154ba4c5bd9
abstractnk <nandakishore01995@gmail.com>
2019-07-14 18:48:25 +0530

    test code push 4

--- src/main/java/utility/setup.java
+++ src/main/java/utility/setup.java
@@ -2,6 +2,8 @@ package utility;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class setup {
@@ -11,7 +13,10 @@ public class setup {
 	public WebDriver getChromeDriver()
 	{
 		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--no-sandbox");
+		driver = new ChromeDriver(chromeOptions);
 		driver.manage().window().maximize();
 		return driver;
 	}
